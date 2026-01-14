eed8ea5ad76fadba50db6ddf471693c89e3e8cae
Knappkin <44062671+Knappkin@users.noreply.github.com>
2026-01-14 12:56:33 -0500

    Created FSM, states, Blobert stats
    created the FSM for blobert. Made a script to store blobert's stats to be controlled by the actions, as well as for collision detection of kibble. made a kibble prefab, box collider on kibble acts as trigger

---
+++ Assets/Scripts/BlobertStats.cs
@@ -0,0 +1,31 @@
+using System.Drawing;
+using UnityEngine;
+
+public class BlobertStats : MonoBehaviour
+{
+    
+    // starting/max stats
+    public float maxEnergy;
+    public float startSize;
+    public float maxSize;
+    public float wakeUpThreshold;
+
+    // changing stats
+    public float energy;
+    public float size;
+
+    public bool canEat;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
