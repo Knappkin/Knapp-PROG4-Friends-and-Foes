05214ae32c18ede97dd3b5c07f0cdcea59497860
Knappkin <44062671+Knappkin@users.noreply.github.com>
2026-02-03 19:06:21 -0500

    Some of week 5

--- Assets/Week 4&5/ClickToMove.cs
+++ Assets/Week 4&5/ClickToMove.cs
@@ -5,11 +5,16 @@ using UnityEngine.InputSystem;
 public class ClickToMove : MonoBehaviour
 {
     public NavMeshAgent navAgent;
+    public LayerMask terrainLayers;
     private InputSystem_Actions inputActions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
         inputActions = new InputSystem_Actions();
+    }
+    private void OnEnable()
+    {
         inputActions.Enable();
 
         inputActions.Player.Attack.performed += OnAttack;
@@ -17,11 +22,17 @@ public class ClickToMove : MonoBehaviour
 
     private void OnAttack(InputAction.CallbackContext context)
     {
-        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray mouseRay = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
 
-        if (Physics.Raycast(mouseRay, out RaycastHit hitInfo))
+        if (Physics.Raycast(mouseRay, out RaycastHit hitInfo, float.MaxValue, terrainLayers))
         {
             navAgent.SetDestination(hitInfo.point);
         }
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Attack.performed -= OnAttack;
+        inputActions.Disable();
     }
 }
---
+++ Assets/Week 4&5/Tasks/Actions/NavigationAT.cs
@@ -0,0 +1,60 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+namespace NodeCanvas.Tasks.Actions {
+
+	public class NavigationAT : ActionTask {
+
+		public BBParameter<Vector3> targetPositionBBP;
+		public BBParameter<float> timeSinceLastSampleBBP;
+		public BBParameter<bool> isMovingBBP;
+
+		public float sampleRateInSeconds;
+		public float sampleRadiusInUnits;
+
+		private Vector3 lastTargetPosition;
+		private NavMeshAgent navAgent;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit() {
+			navAgent = agent.GetComponent<NavMeshAgent>();
+
+			if (navAgent == null)
+			{
+				return $"{agent.name} - NavigationTask: Unable to get NavMesh Agent Reference!";
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		//Called once per frame while the action is active.
+		protected override void OnUpdate() {
+
+			timeSinceLastSampleBBP.value += Time.deltaTime;
+			if (timeSinceLastSampleBBP.value > sampleRateInSeconds)
+			{
+				timeSinceLastSampleBBP.value = 0;
+
+				if (lastTargetPosition != targetPositionBBP.value)
+				{
+					lastTargetPosition = targetPositionBBP.value;
+					if (NavMesh.SamplePosition(targetPositionBBP.value, out NavMeshHit hitInfo, sampleRadiusInUnits, NavMesh.AllAreas))
+					{
+						navAgent.SetDestination(hitInfo.position);
+					}
+				}
+
+				isMovingBBP.value =
+					navAgent.remainingDistance != 0 &&
+					navAgent.remainingDistance != Mathf.Infinity ||
+					navAgent.pathPending;
+			}
+		}
+	}
+}
